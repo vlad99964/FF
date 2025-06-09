df75526866a2ba30780bee0da31b5a0774e118ac
vlad99964 <vlad99964@yandex.ru>
2025-06-09 18:54:02 +0300

    0.01

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template_string, request, redirect, session, url_for
 import sqlite3, hashlib, os, glob
 from werkzeug.utils import secure_filename
-
+# нагадил тут
 app = Flask(__name__)
 app.secret_key = "your_secret_key_here"
 UPLOAD_FOLDER = "static/uploads"
